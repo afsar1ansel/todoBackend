91d8f0bc345058940f100a2d3c9ba464ac586056
md afsar <mdafsaransal@gmail.com>
2024-01-31 19:12:49 +0530

    swagger

--- index.js
+++ index.js
@@ -5,21 +5,44 @@ const noteRouter = require("./routes/noteRoutes");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
+// Swagger docs setup
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Todo Backend",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:7777",
+      },
+    ],
+  },
+  apis: ["index.js", "./routes/userRouter.js", "./routes/noteRoutes.js"],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+// Express app setup
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(express.json());
-
 app.use(cookieParser("token"));
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
-
+// Root endpoint
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-
+// Server start
 app.listen(7777, async () => {
-    await dbConnect();
-    console.log("server is running on port 7777");
-})
+  await dbConnect();
+  console.log("Server is running on port 7777");
+});
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -5,6 +5,168 @@ const jwt = require("jsonwebtoken");
 const {auth} = require("../middleware/auth.middleware");
 const Try = require("../middleware/try.middleware");
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      NoteResponse:
+ *          type: object
+ *          properties:
+ *              message:
+ *                  type: string
+ *                  description: A message indicating the status of the operation
+ *              note:
+ *                  $ref: '#/components/schemas/Note'
+ * 
+ *      Note:
+ *          type: object
+ *          properties:
+ *              title:
+ *                  type: string
+ *                  description: Title of the note
+ *              description:
+ *                  type: string
+ *                  description: Description of the note
+ *              userID:
+ *                  type: string
+ *                  description: ID of the user associated with the note
+ *              name:
+ *                  type: string
+ *                  description: Name of the user associated with the note
+ * 
+ *  responses:
+ *      UnauthorizedError:
+ *          type: object
+ *          properties:
+ *              message:
+ *                  type: string
+ *                  description: Error message indicating unauthorized access
+ * 
+ *      NotFoundError:
+ *          type: object
+ *          properties:
+ *              message:
+ *                  type: string
+ *                  description: Error message indicating that the resource was not found
+ * 
+*/
+
+/**
+ * @swagger
+ * /note/add:
+ *  post:
+ *      summary: Add a new note
+ *      tags: [Notes]
+ *      security:
+ *        - BearerAuth: []
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Note'
+ *      responses:
+ *        200:
+ *          description: Note added successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/NoteResponse'
+ *        401:
+ *          discription: Unauthorized access
+ */
+
+/**
+ * @swagger
+ * /note:
+ *  get:
+ *      summary: Get all notes for a user
+ *      tags: [Notes]
+ *      security:
+ *        - BearerAuth: []
+ *      responses:
+ *        200:
+ *          description: List of notes retrieved successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Note'
+ *        401:
+ *         discription: Unauthorized access
+ */
+
+/**
+ * @swagger
+ * /note/update/{id}:
+ *  patch:
+ *      summary: Update a note by ID
+ *      tags: [Notes]
+ *      security:
+ *        - BearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          description: ID of the note to be updated
+ *          schema:
+ *            type: string
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                title:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          description: Note updated successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/NoteResponse'
+ *        401:
+ *          discription: Unauthorized access
+ *        404:
+ *          discription: Note not found
+ */
+
+/**
+ * @swagger
+ * /note/delete/{id}:
+ *  delete:
+ *      summary: Delete a note by ID
+ *      tags: [Notes]
+ *      security:
+ *        - BearerAuth: []
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          description: ID of the note to be deleted
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: Note deleted successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/NoteResponse'
+ *        401:
+ *          discription: Unauthorized access
+ *        404:
+ *         discription: Note not found
+ */
+
+
+
+
 noteRouter.post("/add", auth, async (req, res) => {
   const { title, description ,userID,name } = req.body;
   try {
